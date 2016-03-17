a1db48c212c38ddc83fae776d019f45986551fd3
Alexander <alexander.zybin@gmail.com>
2016-03-17 10:04:48 +0300

    sdfsdfsdf

--- src/block/site/default/_history-back/.document-ready.js
+++ src/block/site/default/_history-back/.document-ready.js
@@ -1,5 +1,13 @@
 	
+	/*
 	$('.history-back').on('click', function(event){
+		event.preventDefault();
+		
+		window.history.back();
+	});
+	*/
+	
+	$(document.body).on('click.fecss.history-back', '.history-back', function(event){
 		event.preventDefault();
 		
 		window.history.back();
